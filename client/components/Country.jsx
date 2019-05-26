efe14c4e7e4f0e812eead022e3f3e7b32c32419e
Drew del Rosario <drewdelrosario@gmail.com>
2019-05-25 18:46:46 -0700

    Cleans country component

--- client/components/Country.jsx
+++ client/components/Country.jsx
@@ -2,24 +2,17 @@ import React from 'react';
 import USA from 'react-svg-loader!../Images/USA.svg';
 import Can from 'react-svg-loader!../Images/Canada.svg';
 
-const Country = props => {
-  let countryRender;
-  // console.log(props.country);
-  if (props.country === 'US') {
-    countryRender = (
-      <span className="row">
-        <USA height={15} width={15} />
-        <div>US</div>
-      </span>
-    );
-  } else {
-    countryRender = (
+const Country = ({ country }) => {
+  const countryRender = country === 'US' ? (
+    <span className="row">
+      <USA height={15} width={15} />
+      <div>US</div>
+    </span>) : (
       <span className="row">
         <Can height={15} width={15} />
         <div>Can</div>
       </span>
     );
-  }
   return <span>{countryRender}</span>;
 };
 export default Country;
